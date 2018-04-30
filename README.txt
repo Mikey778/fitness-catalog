ae66f99451f59849815abcdb683dba0abe4e4080
Mike778 <michaelcharojr@gmail.com>
2018-04-29 18:02:11 -0700

    Initial commit

---
+++ README.txt
@@ -0,0 +1,36 @@
+README
+------
+
+My Fitness Application
+This site is a catalog of Excercises sorted by muscle groups
+Exercises have a description and video instructions
+
+User must be signed in using Google plus to create, edit, or delete an item
+
+API's
+- http://localhost:8000/api/musclegroup
+- http://localhost:8000/api/musclegroup/<musclegroup-id>
+
+Requirements
+-python 2.7
+-CSS
+-HTML
+-Bootstrap 4
+-Google Oauth2 api
+
+Getting Started
+1) Unzip charo_catalog_app.zip
+2) run command below to install required python library's:
+   $ pip install -r requirements.txt
+2) Start the web application with:
+    - catalog/python web_application.py
+3) web application url: http://localhost:8000/
+
+Authors
+Michael Charo
+
+Sources
+Instructional videos embed from Youtube.com
+Exercise instructions from: https://www.bodybuilding.com/
+https://github.com/udacity/ud330/tree/master/Lesson2/step6
+https://developers.google.com/identity/protocols/OAuth2
---
+++ fitness_database.py
@@ -0,0 +1,50 @@
+from sqlalchemy import Column, ForeignKey, Integer, String, Text
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship
+from sqlalchemy import create_engine
+
+Base = declarative_base()
+
+class MuscleGroup(Base):
+    __tablename__ = 'muscle_group'
+
+    id = Column(Integer, primary_key=True)
+    muscle_group_name = Column(String(250), nullable=False)
+
+    @property
+    def serialize(self):
+        """ Return JSON Object """
+        output =    {
+                        'muscle_group_name': self.muscle_group_name,
+                        'id': self.id,
+                    }
+        return output
+
+
+class Exercise(Base):
+    __tablename__ = 'exercises'
+
+    name = Column(String(80), nullable=False)
+    id = Column(Integer, primary_key=True)
+    instructions = Column(Text)
+    video_link = Column(String(250))
+
+    muscle_group_id = Column(Integer, ForeignKey('muscle_group.id'))
+    muscle_group = relationship(MuscleGroup)
+
+    @property
+    def serialize(self):
+        """ Return JSON Object """
+        return {
+            'name': self.name,
+            'instructions': self.instructions,
+            'id': self.id,
+            'video_link': self.video_link,
+        }
+
+engine = create_engine('sqlite:///fitness.db')
+
+Base.metadata.create_all(engine)
+
+
+
